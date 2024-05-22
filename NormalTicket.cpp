0d103dc3184de574133a0d1bf7356957b48f0293
Grzegorz Rzepiela <147431127+grzechuzz@users.noreply.github.com>
2024-05-22 12:43:23 +0200

    Revert "Dodanie klasy Person i Ticket"

--- NormalTicket.cpp
+++
@@ -1,9 +0,0 @@
-#include "NormalTicket.h"
-
-NormalTicket::NormalTicket(const Person& ticketHolder, double price, const std::string& sector, int seat)
-    : Ticket(ticketHolder, price, sector, seat) {}
-
-std::string NormalTicket::showInfo() const {
-    return "Bilet normalny\n" +
-        Ticket::showInfo();
-}
